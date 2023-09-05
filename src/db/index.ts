3b364de901e9ae6f2766d5d79303d093383268bc
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-09-05 16:34:59 +0530

    create user schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,11 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless'
 
 import { connect } from '@planetscale/database'
 
+import * as schema from './schema'
+
 const connection = connect({
   url: env.DATABASE_URL,
 })
 
-export const db = drizzle(connection)
+export const db = drizzle(connection, { schema })
+export const dbSchema = schema
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,35 @@
-import { mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
+import { relations } from 'drizzle-orm'
+import { mysqlTable, varchar } from 'drizzle-orm/mysql-core'
 
-export const countries = mysqlTable('users', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
+export const users = mysqlTable('users', {
+  id: varchar('id', { length: 26 }).primaryKey(),
+  firstName: varchar('first_name', { length: 256 }).notNull(),
+  lastName: varchar('last_name', { length: 256 }),
+  email: varchar('email', { length: 256 }).unique(),
+  profilePicture: varchar('profile_picture', { length: 256 }),
 })
+
+export const credentialAuth = mysqlTable('credential_auth', {
+  id: varchar('id', { length: 26 }).primaryKey(),
+  userId: varchar('user_id', { length: 26 }).notNull(),
+  password: varchar('password', { length: 256 }).notNull(),
+})
+
+export const googleAuth = mysqlTable('google_auth', {
+  id: varchar('id', { length: 26 }).primaryKey(),
+  userId: varchar('user_id', { length: 26 }).notNull(),
+  googleId: varchar('google_id', { length: 256 }).notNull(),
+})
+
+export const userRelations = relations(users, ({ one }) => ({
+  credentialAuth: one(credentialAuth),
+  googleAuth: one(googleAuth),
+}))
+
+export const credentialAuthRelations = relations(credentialAuth, ({ one }) => ({
+  user: one(users),
+}))
+
+export const googleAuthRelations = relations(googleAuth, ({ one }) => ({
+  user: one(users),
+}))
