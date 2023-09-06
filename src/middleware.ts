91916e96e7436abacd383a8f6e9ac984a49fea7d
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-09-06 15:57:35 +0530

    add auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { env } from '#env'
+import * as jose from 'jose'
+
+export async function middleware(req: NextRequest) {
+  try {
+    const url = req.url
+    const token = req.cookies.get('auth')?.value
+    const isAuth = await checkIsAuth(token)
+
+    const isIndexPage = req.nextUrl.pathname === '/'
+
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith('/login') ||
+      req.nextUrl.pathname.startsWith('/register')
+
+    const isAppPage =
+      req.nextUrl.pathname.startsWith('/today') ||
+      req.nextUrl.pathname.startsWith('/user')
+
+    if (isAuth) {
+      if (isIndexPage) {
+        return NextResponse.rewrite(new URL('/today', url))
+      }
+
+      if (isAuthPage) {
+        return NextResponse.redirect(new URL('/', url))
+      }
+    }
+
+    if (!isAuth) {
+      if (isIndexPage) {
+        return NextResponse.rewrite(new URL('/home', url))
+      }
+
+      if (isAppPage) {
+        return NextResponse.redirect(new URL('/login', url))
+      }
+    }
+
+    return null
+  } catch (error) {
+    return null
+  }
+}
+
+export const config = {
+  matcher: [
+    '/',
+
+    '/login/:path*',
+    '/register/:path*',
+
+    '/today/:path*',
+    '/user/:path*',
+  ],
+}
+
+async function checkIsAuth(token?: string) {
+  try {
+    if (!token) return false
+    const secret = jose.base64url.decode(env.JWT_SECRET)
+    const { payload } = await jose.jwtDecrypt(token, secret)
+    if (!payload) return false
+    if (!payload?.userId) return false
+    if (typeof payload.userId !== 'string') return false
+    return true
+  } catch (error) {
+    return false
+  }
+}
