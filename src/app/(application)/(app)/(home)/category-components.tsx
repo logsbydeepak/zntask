176e0a8d680153a656b48f88a3662f804242eb11
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-09-19 20:20:00 +0530

    add menu in category item

--- src/app/(application)/(app)/(home)/category-components.tsx
+++ src/app/(application)/(app)/(home)/category-components.tsx
@@ -1,15 +1,27 @@
 import Link from 'next/link'
-import { EditIcon, HeartIcon, HeartOffIcon, Trash2Icon } from 'lucide-react'
-
 import {
-  ContextMenuItem,
-  DropdownMenuItem,
-  MenuIcon,
-} from '@/components/ui/menu'
+  EditIcon,
+  HeartIcon,
+  HeartOffIcon,
+  MoreVerticalIcon,
+  Trash2Icon,
+} from 'lucide-react'
+
 import { useAppStore } from '@/store/app'
 import { Category, useCategoryStore } from '@/store/category'
 import { getCategoryColor } from '@/utils/category'
 import { cn } from '@/utils/style'
+import {
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuRoot,
+  ContextMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+  MenuIcon,
+} from '@ui/menu'
 
 export function CategoryItem({
   category,
@@ -19,18 +31,42 @@ export function CategoryItem({
   href: string
 }) {
   return (
-    <Link
-      href={href}
-      className="flex items-center space-x-3 rounded-lg border border-transparent px-4 py-2 hover:border-gray-200 hover:bg-gray-50"
-    >
-      <div
-        className={cn(
-          'h-3 w-3 rounded-sm',
-          `bg-${getCategoryColor(category.indicator)}-600`
-        )}
-      />
-      <p className="text-sm">{category.title}</p>
-    </Link>
+    <ContextMenuRoot>
+      <DropdownMenuRoot>
+        <ContextMenuTrigger className="group">
+          <Link
+            href={href}
+            className="flex items-center justify-between rounded-lg border border-transparent px-4 py-2 hover:border-gray-200 hover:bg-gray-50"
+          >
+            <div className="flex items-center space-x-3">
+              <div
+                className={cn(
+                  'h-3 w-3 rounded-sm',
+                  `bg-${getCategoryColor(category.indicator)}-600`
+                )}
+              />
+              <p className="text-sm">{category.title}</p>
+            </div>
+            <div>
+              <DropdownMenuTrigger asChild>
+                <button className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-800 data-[state=open]:text-gray-800">
+                  <span className="inline-block h-4 w-4">
+                    <MoreVerticalIcon className="h-full w-full" />
+                  </span>
+                </button>
+              </DropdownMenuTrigger>
+            </div>
+          </Link>
+        </ContextMenuTrigger>
+
+        <ContextMenuContent>
+          <CategoryMenuContent category={category} type="context" />
+        </ContextMenuContent>
+        <DropdownMenuContent align="end">
+          <CategoryMenuContent category={category} type="dropdown" />
+        </DropdownMenuContent>
+      </DropdownMenuRoot>
+    </ContextMenuRoot>
   )
 }
 
