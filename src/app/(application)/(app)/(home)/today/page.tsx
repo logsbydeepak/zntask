702b2c9d3ec4aaca15d81b6d32033eca444a8ad1
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-10-03 18:46:58 +0530

    show today's schedule task

--- src/app/(application)/(app)/(home)/today/page.tsx
+++ src/app/(application)/(app)/(home)/today/page.tsx
@@ -1,9 +1,22 @@
+'use client'
+
+import { isToday } from 'date-fns'
 import { CalendarClockIcon } from 'lucide-react'
+import { useShallow } from 'zustand/shallow'
 
 import * as Layout from '@/app/(application)/(app)/layout-components'
 import { Head } from '@/components/head'
+import { useAppStore } from '@/store/app'
+import { useTaskStore } from '@/store/task'
 
 export default function Page() {
+  const tasks = useTaskStore(
+    useShallow((s) =>
+      s.tasks.filter((i) => i.date && isToday(new Date(i.date)))
+    )
+  )
+  const setDialog = useAppStore((s) => s.setDialog)
+
   return (
     <Layout.Root>
       <Layout.Header>
@@ -11,12 +24,20 @@ export default function Page() {
         <Head title="Today" />
       </Layout.Header>
       <Layout.Content>
-        <Layout.Empty.Container>
-          <Layout.Empty.Icon>
-            <CalendarClockIcon className="h-full w-full" />
-          </Layout.Empty.Icon>
-          <Layout.Empty.Label>No task</Layout.Empty.Label>
-        </Layout.Empty.Container>
+        {tasks.length === 0 && (
+          <Layout.Empty.Container>
+            <Layout.Empty.Icon>
+              <CalendarClockIcon className="h-full w-full" />
+            </Layout.Empty.Icon>
+            <Layout.Empty.Label>No task</Layout.Empty.Label>
+          </Layout.Empty.Container>
+        )}
+
+        {tasks.map((i) => (
+          <p key={i.id} onClick={() => setDialog('editTask', i)}>
+            {i.title}
+          </p>
+        ))}
       </Layout.Content>
     </Layout.Root>
   )
