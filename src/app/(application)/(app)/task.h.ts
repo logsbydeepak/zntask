482043bfe2a5c49a53686635bff42eb28d434cf9
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-10-17 19:07:44 +0530

    create parent and child task create action

--- src/app/(application)/(app)/task.h.ts
+++ src/app/(application)/(app)/task.h.ts
@@ -1,5 +1,9 @@
-import { db } from '@/db'
+import { isValid } from 'ulidx'
+import { z } from 'zod'
+
+import { db, dbSchema } from '@/db'
 import { h, r } from '@/utils/handler'
+import { zRequired } from '@/utils/zod'
 
 export const getTasks = h('AUTH', async ({ userId }) => {
   // const tasks = await db.query.tasks.findMany({
@@ -19,3 +23,42 @@ export const getTasks = h('AUTH', async ({ userId }) => {
   // return r('OK', { tasks: modTasks })
   return r('OK', { tasks: [] })
 })
+
+const zTask = z.object({
+  id: zRequired.refine(isValid, { message: 'Invalid ulid' }),
+  title: zRequired,
+  isCompleted: z.boolean(),
+  orderId: zRequired.refine(isValid, { message: 'Invalid ulid' }),
+  date: z.string().nullable(),
+  time: z.string().nullable(),
+  details: z.string().nullable(),
+})
+
+const zParentTask = z.object({
+  ...zTask.shape,
+  categoryId: zRequired.refine(isValid, { message: 'Invalid ulid' }).nullable(),
+})
+
+const zChildTask = z.object({
+  ...zTask.shape,
+  parentId: zRequired.refine(isValid, { message: 'Invalid ulid' }),
+})
+
+export const createParentTask = h(
+  'AUTH',
+  zParentTask,
+  async ({ userId, input }) => {
+    await db.insert(dbSchema.parentTasks).values({ ...input, userId })
+
+    return r('OK', { input })
+  }
+)
+
+export const createChildTask = h(
+  'AUTH',
+  zChildTask,
+  async ({ userId, input }) => {
+    await db.insert(dbSchema.childTask).values({ ...input, userId })
+    return r('OK', { input })
+  }
+)
