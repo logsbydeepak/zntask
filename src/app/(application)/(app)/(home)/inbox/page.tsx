375c4b961310fd1e460c67c79932343ee1d624e9
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-10-24 14:56:46 +0530

    refactor parent and child task in inbox page

--- src/app/(application)/(app)/(home)/inbox/page.tsx
+++ src/app/(application)/(app)/(home)/inbox/page.tsx
@@ -7,7 +7,7 @@ import { useShallow } from 'zustand/react/shallow'
 import * as Layout from '@/app/(application)/(app)/layout-components'
 import { Head } from '@/components/head'
 import { useAppStore } from '@/store/app'
-import { ChildTask, ParentTask, useTaskStore } from '@/store/task'
+import { ParentTask, Task, useTaskStore } from '@/store/task'
 
 export default function Page() {
   const tasks = useTaskStore(
@@ -32,7 +32,7 @@ export default function Page() {
 
         <div className="space-y-3">
           {tasks.map((i) => (
-            <ParentTaskItem key={i.id} task={i} />
+            <TaskContainer key={i.id} task={i} />
           ))}
         </div>
       </Layout.Content>
@@ -40,49 +40,45 @@ export default function Page() {
   )
 }
 
-function ParentTaskItem({ task }: { task: ParentTask }) {
-  const [isCollapsibleOpen, setIsCollapsibleOpen] = React.useState(false)
+function TaskContainer({ task }: { task: ParentTask }) {
   const childTask = useTaskStore(
     useShallow((s) => s.childTasks.filter((i) => i.parentId === task.id))
-  )
-  const setDialog = useAppStore((s) => s.setDialog)
-
-  const childTaskToDisplay = childTask.slice(
-    0,
-    childTask.length >= 5 && !isCollapsibleOpen ? 4 : childTask.length
   )
 
   return (
     <div>
-      <button
-        className="w-full text-left"
-        onClick={() => {
-          setDialog({ editTask: { parentTaskId: task.id } })
-        }}
-      >
-        <p>{task.title}</p>
-      </button>
-      <div className="ml-5">
-        {childTaskToDisplay.map((i) => (
-          <ChildTaskItem key={i.id} task={i} />
+      <TaskItem task={task} />
+      <div className="ml-4">
+        {childTask.map((i) => (
+          <TaskItem key={i.id} task={i} isChildTask={true} />
         ))}
-        <button onClick={() => setIsCollapsibleOpen((open) => !open)}>
-          toggle show
-        </button>
       </div>
     </div>
   )
 }
 
-function ChildTaskItem({ task }: { task: ChildTask }) {
+function TaskItem({
+  task,
+  isChildTask = false,
+}: {
+  task: Task
+  isChildTask?: boolean
+}) {
   const setDialog = useAppStore((s) => s.setDialog)
 
+  const handleOnTaskClick = () => {
+    if (isChildTask) {
+      setDialog({ editTask: { childTaskId: task.id } })
+      return
+    }
+    setDialog({ editTask: { parentTaskId: task.id } })
+  }
+
   return (
-    <button
-      className="block text-xs"
-      onClick={() => setDialog({ editTask: { childTaskId: task.id } })}
-    >
-      <p>{task.title}</p>
-    </button>
+    <div>
+      <button onClick={handleOnTaskClick}>
+        <p>{task.title}</p>
+      </button>
+    </div>
   )
 }
