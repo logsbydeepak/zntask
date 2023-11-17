4961735ab58554cb3fd89f7b4180c3b5644edf39
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-11-17 20:35:56 +0530

    show hovering indicator

--- src/app/(application)/(app)/(home)/category.tsx
+++ src/app/(application)/(app)/(home)/category.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import Link from 'next/link'
-import { useDrag } from '@use-gesture/react'
+import { useDrag, useGesture } from '@use-gesture/react'
 import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai'
 import {
   ArchiveRestoreIcon,
@@ -11,6 +11,7 @@ import {
   MoreVerticalIcon,
   Trash2Icon,
 } from 'lucide-react'
+import { useDebounce } from 'use-debounce'
 
 import { JotaiProvider } from '@/components/client-providers'
 import {
@@ -31,13 +32,26 @@ import { useCategoryStore } from '@/store/category'
 import { Category, getCategoryColor } from '@/utils/category'
 import { cn } from '@/utils/style'
 
-const allDroppableContainerAtom = atom<(DOMRect & { id: string })[]>([])
-const draggingContainerAtom = atom<{
-  id: string
-  x: number
-  y: number
-  rect: DOMRect
-} | null>(null)
+interface Rec {
+  left: number
+  top: number
+  width: number
+  height: number
+}
+
+function centerOfRectangle(rect: Rec) {
+  return {
+    x: rect.left + rect.width * 0.5,
+    y: rect.top + rect.height * 0.5,
+  }
+}
+
+function distance(a: { x: number; y: number }, b: { x: number; y: number }) {
+  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+}
+
+const allDroppableContainerAtom = atom<(Rec & { id: string })[]>([])
+const currentDroppableContainerHoverAtom = atom<string | null>(null)
 
 export function CategoryItem({
   category,
@@ -47,44 +61,91 @@ export function CategoryItem({
   href: string
 }) {
   const id = category.id
-  const [preventFocus, setPreventFocus] = React.useState(false)
-  const [isHovering, setIsHovering] = React.useState(false)
-  const ref = React.useRef<HTMLAnchorElement>(null)
-  const setAllDroppableContainer = useSetAtom(allDroppableContainerAtom)
-  const setDraggingContainer = useSetAtom(draggingContainerAtom)
-  const dragglingContainer = useAtomValue(draggingContainerAtom)
+
+  const [allDroppableContainer, setAllDroppableContainer] = useAtom(
+    allDroppableContainerAtom
+  )
+  const [currentDroppableContainerHover, setCurrentDroppableContainerHover] =
+    useAtom(currentDroppableContainerHoverAtom)
+
   const [isDragging, setIsDragging] = React.useState(false)
+  const [position, setPosition] = React.useState({ x: 0, y: 0 })
+  const [differPosition] = useDebounce(position, 50)
+  const [preventFocus, setPreventFocus] = React.useState(false)
+  const ref = React.useRef<HTMLAnchorElement>(null)
+
+  const isHovering = currentDroppableContainerHover === id
 
   const bind = useDrag(
     ({ down, movement: [mx, my] }) => {
-      setDraggingContainer(() => {
-        setIsDragging(down)
-        if (!down) {
-          return null
-        }
-
-        const el = ref.current?.getBoundingClientRect()
-        if (!el) return null
-        return {
-          id,
-          x: mx,
-          y: my,
-          rect: el,
-        }
-      })
+      setIsDragging(down)
+      if (down) {
+        setPosition({ x: mx, y: my })
+      } else {
+        setPosition({ x: 0, y: 0 })
+        setCurrentDroppableContainerHover(null)
+      }
     },
     { preventDefault: true, filterTaps: true }
   )
 
   useEffect(() => {
     const el = ref.current?.getBoundingClientRect()
+    if (differPosition.x === 0 && differPosition.y === 0) return
     if (!el) return
-    setAllDroppableContainer((prev) => [...prev, { ...el, id }])
-  }, [setAllDroppableContainer])
+    if (!isDragging) return
 
-  const style = isDragging && {
-    transform: `translate3d(${dragglingContainer?.x}px, ${dragglingContainer?.y}px, 0)`,
-  }
+    const items = allDroppableContainer.filter((i) => i.id !== id)
+
+    const centerOfDroppableContainer = items.map((i) => ({
+      center: centerOfRectangle(i),
+      id: i.id,
+    }))
+
+    const centerOfCurrentDragging = centerOfRectangle({
+      left: el.left,
+      top: el.top,
+      width: el.width,
+      height: el.height,
+    })
+
+    const distanceBetween = centerOfDroppableContainer.map((i) => ({
+      distance: distance(i.center, {
+        x: centerOfCurrentDragging.x,
+        y: centerOfCurrentDragging.y,
+      }),
+      id: i.id,
+    }))
+
+    const closest = distanceBetween.reduce((prev, curr) =>
+      prev.distance < curr.distance ? prev : curr
+    )
+
+    setCurrentDroppableContainerHover(closest.id)
+  }, [
+    position,
+    allDroppableContainer,
+    differPosition,
+    id,
+    isDragging,
+    setCurrentDroppableContainerHover,
+  ])
+
+  useEffect(() => {
+    const el = ref.current?.getBoundingClientRect()
+    if (!el) return
+
+    setAllDroppableContainer((prev) => [
+      ...prev,
+      {
+        id,
+        left: el.left,
+        top: el.top,
+        height: el.height,
+        width: el.width,
+      },
+    ])
+  }, [setAllDroppableContainer, id])
 
   return (
     <ContextMenuRoot>
@@ -94,10 +155,9 @@ export function CategoryItem({
             {...bind()}
             ref={ref}
             href={href}
-            style={{ ...style }}
-            // style={{
-            //   transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
-            // }}
+            style={{
+              transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+            }}
             className={cn(
               'relative flex touch-none items-center justify-between rounded-lg border border-transparent px-4 py-2 hover:border-gray-200 hover:bg-gray-50 group-data-[state=open]:border-gray-200 group-data-[state=open]:bg-gray-50',
               isDragging && 'z-50'
@@ -113,7 +173,7 @@ export function CategoryItem({
                 />
               </div>
               <p className="select-none overflow-hidden text-ellipsis text-sm">
-                {category.title}
+                {category.title} {category.id}
               </p>
             </div>
             <div className="flex items-center space-x-1">
@@ -169,6 +229,7 @@ function Indicator() {
 export function CategoryContainer({ children }: { children: React.ReactNode }) {
   return (
     <JotaiProvider>
+      <div className="fixed left-0 top-0 z-50 h-2 w-2 bg-red-800"></div>
       <div className="space-y-2">{children}</div>
     </JotaiProvider>
   )
