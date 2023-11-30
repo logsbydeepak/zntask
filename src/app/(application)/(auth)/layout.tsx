c07192c8d5ce29423ad1be3c84ccd3cbe8c2fd78
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-11-30 18:42:30 +0530

    drag to dismiss toast

--- src/app/(application)/(auth)/layout.tsx
+++ src/app/(application)/(auth)/layout.tsx
@@ -5,10 +5,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Navbar />
       <div className="w-full items-center justify-center md:flex">
-        <div className="w-full border-gray-100 md:my-36 md:w-[500px] md:rounded-md md:border md:shadow-sm md:drop-shadow-sm">
+        <div className="w-full border-gray-100 bg-white md:my-[100px] md:w-[500px] md:rounded-md md:border md:shadow-sm md:drop-shadow-sm">
           {children}
         </div>
       </div>
+      <div className="pattern fixed inset-0 -z-10 bg-gray-50/50" />
     </>
   )
 }
