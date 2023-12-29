b63c47792f297e86b1d64e43c75e9b6bb796ccb4
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-12-29 17:16:56 +0530

    add animation in menu

--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -17,7 +17,7 @@ import {
 } from '@radix-ui/react-dropdown-menu'
 import { cva } from 'cva'
 
-import { cn, tw } from '@/utils/style'
+import { cn } from '@/utils/style'
 
 export const ContextMenuRoot = ContextMenuRootPrimitives
 export const ContextMenuTrigger = ContextMenuTriggerPrimitives
@@ -29,7 +29,12 @@ export const DropdownMenuTrigger = DropdownMenuTriggerPrimitives
 export const DropdownMenuRadioGroup = DropdownMenuRadioGroupPrimitives
 export const DropdownMenuRadioItem = DropdownMenuRadioItemPrimitives
 
-const menuContentStyle = tw`z-40 rounded-xl border border-gray-200 bg-white p-1 drop-shadow-sm`
+const menuContentStyle = cn(
+  'z-40 rounded-xl border border-gray-200 bg-white p-1 drop-shadow-sm animate-in fade-in-80',
+  'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
+  'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
+  'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2'
+)
 
 const menuItemStyle = cva({
   base: 'group/item flex items-center space-x-3 rounded-md bg-white px-4 py-2 text-xs font-medium focus:outline-none data-[highlighted]:cursor-pointer',
