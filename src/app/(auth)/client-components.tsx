8fe8a504c20996e798e3482880dcd784f0491839
deepak <96181928+logsbydeepak@users.noreply.github.com>
2023-12-30 17:31:15 +0530

    add radio group in theme

--- src/app/(auth)/client-components.tsx
+++ src/app/(auth)/client-components.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import React from 'react'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 import { MonitorIcon, MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import * as Tooltip from '@/components/ui/tooltip'
-import { cn } from '@/utils/style'
 
 export function Theme() {
   const { theme: themeProvider, setTheme: setThemeProvider } = useTheme()
@@ -20,61 +20,60 @@ export function Theme() {
   }
 
   return (
-    <div className="inline-flex space-x-2 self-center rounded-full border px-1 py-1">
-      <Tooltip.Provider>
+    <Tooltip.Provider>
+      <RadioGroup.Root
+        value={theme}
+        onValueChange={setTheme}
+        className="flex space-x-1 self-center rounded-full border border-gray-200 p-1"
+      >
         <Tooltip.Root>
           <Tooltip.Trigger asChild>
-            <Icon
-              isActive={theme === 'light'}
-              onClick={() => setTheme('light')}
-            >
-              <SunIcon />
-            </Icon>
+            <RadioGroup.Item value="light" asChild>
+              <Icon>
+                <SunIcon />
+              </Icon>
+            </RadioGroup.Item>
           </Tooltip.Trigger>
-          <Tooltip.Content>light theme</Tooltip.Content>
+          <Tooltip.Content sideOffset={8}>light theme</Tooltip.Content>
         </Tooltip.Root>
 
         <Tooltip.Root>
           <Tooltip.Trigger asChild>
-            <Icon isActive={theme === 'dark'} onClick={() => setTheme('dark')}>
-              <MoonIcon />
-            </Icon>
+            <RadioGroup.Item value="dark" asChild>
+              <Icon>
+                <MoonIcon />
+              </Icon>
+            </RadioGroup.Item>
           </Tooltip.Trigger>
-          <Tooltip.Content>dark theme</Tooltip.Content>
+          <Tooltip.Content sideOffset={8}>dark theme</Tooltip.Content>
         </Tooltip.Root>
 
         <Tooltip.Root>
           <Tooltip.Trigger asChild>
-            <Icon
-              isActive={theme === 'system'}
-              onClick={() => setTheme('system')}
-            >
-              <MonitorIcon />
-            </Icon>
+            <RadioGroup.Item value="system" asChild>
+              <Icon>
+                <MonitorIcon />
+              </Icon>
+            </RadioGroup.Item>
           </Tooltip.Trigger>
-          <Tooltip.Content>system theme</Tooltip.Content>
+          <Tooltip.Content sideOffset={8}>system theme</Tooltip.Content>
         </Tooltip.Root>
-      </Tooltip.Provider>
-    </div>
+      </RadioGroup.Root>
+    </Tooltip.Provider>
   )
 }
 
 const Icon = React.forwardRef<
   HTMLButtonElement,
-  React.ComponentPropsWithoutRef<'button'> & {
-    isActive: boolean
-  }
->(({ children, isActive, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<'button'>
+>(({ children, ...props }, ref) => {
   return (
     <button
       ref={ref}
       {...props}
-      className={cn(
-        'rounded-full p-1',
-        isActive ? 'bg-gray-950 text-white' : 'text-gray-800'
-      )}
+      className="flex size-5 items-center justify-center rounded-full text-gray-600 outline-2 outline-offset-2 outline-gray-950 hover:text-gray-950 focus-visible:outline aria-[checked=true]:bg-gray-950 aria-[checked=true]:text-gray-50"
     >
-      <div className="size-3.5">{children}</div>
+      <div className="size-3">{children}</div>
     </button>
   )
 })
