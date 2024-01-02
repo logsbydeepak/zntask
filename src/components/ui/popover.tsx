3a00fc55e892be22fbb6c901f1a85f42ee77e8ca
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-01-02 20:45:39 +0530

    create popover ui component

---
+++ src/components/ui/popover.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import * as PopoverPrimitive from '@radix-ui/react-popover'
+
+import { cn } from '@/utils/style'
+
+export const Root = PopoverPrimitive.Root
+export const Trigger = PopoverPrimitive.Trigger
+export const Portal = PopoverPrimitive.Portal
+
+export const Content = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <PopoverPrimitive.Content
+    ref={ref}
+    {...props}
+    className={cn(
+      'bg-popover text-popover-foreground z-50 w-72 rounded-md border p-4 shadow-md outline-none',
+      'data-[state=open]:animate-in data-[state=closed]:animate-out',
+      'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+      'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2',
+      'data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      className
+    )}
+  />
+))
+Content.displayName = PopoverPrimitive.Content.displayName
