fee371345dfc4060e7e02bfdb87e8fe7bc19c476
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-01-06 14:54:52 +0530

    add loading state in user page

--- src/app/(app)/(settings)/user/page.tsx
+++ src/app/(app)/(settings)/user/page.tsx
@@ -1,4 +1,6 @@
+import { Suspense } from 'react'
 import { unstable_cache } from 'next/cache'
+import { Loader2Icon, LoaderIcon } from 'lucide-react'
 
 import * as Layout from '@/app/(app)/app-layout'
 import { Head } from '@/components/head'
@@ -19,8 +21,6 @@ const getUserWithAuthData = unstable_cache(
 )
 
 export default async function Page() {
-  const user = await getUserWithAuthData()
-
   return (
     <Layout.Root>
       <Layout.Header>
@@ -28,35 +28,52 @@ export default async function Page() {
         <Head title="User" />
       </Layout.Header>
       <Layout.Content>
-        <div className="pb-8">
-          <table className="border-separate border-spacing-x-2 border-spacing-y-2 text-sm">
-            <tbody className="">
-              <tr>
-                <td>Name</td>
-                <td>{`${user.firstName} ${user.lastName}`}</td>
-              </tr>
-              <tr>
-                <td>Email</td>
-                <td>{user.email}</td>
-              </tr>
-              <tr>
-                <td>Picture</td>
-                <td>{user.profilePicture}</td>
-              </tr>
-              <tr>
-                <td>Google</td>
-                <td>{user.auth.google.toString()}</td>
-              </tr>
-              <tr>
-                <td>Credential</td>
-                <td>{user.auth.credential.toString()}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <Update />
+        <Suspense fallback={<Loading />}>
+          <UserInformation />
+        </Suspense>
       </Layout.Content>
     </Layout.Root>
+  )
+}
+
+function Loading() {
+  return (
+    <div className="flex h-[calc(100vh-200px)] flex-col items-center justify-center">
+      <LoaderIcon className="size-6 animate-spin text-gray-400" />
+    </div>
+  )
+}
+
+async function UserInformation() {
+  const user = await getUserWithAuthData()
+
+  return (
+    <div className="pb-8">
+      <table className="border-separate border-spacing-x-2 border-spacing-y-2 text-sm">
+        <tbody className="">
+          <tr>
+            <td>Name</td>
+            <td>{`${user.firstName} ${user.lastName}`}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <td>Picture</td>
+            <td>{user.profilePicture}</td>
+          </tr>
+          <tr>
+            <td>Google</td>
+            <td>{user.auth.google.toString()}</td>
+          </tr>
+          <tr>
+            <td>Credential</td>
+            <td>{user.auth.credential.toString()}</td>
+          </tr>
+        </tbody>
+      </table>
+      <Update />
+    </div>
   )
 }
