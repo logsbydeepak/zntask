5a5dd3d2d619e00736c97149f64ba30090301c80
deepak <96181928+logsbydeepak@users.noreply.github.com>
2024-04-09 15:17:42 +0530

    add login and register link in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,37 @@
+import Link from 'next/link'
+
+import { LogoIcon } from '@/components/icon/logo'
+import { buttonStyle } from '@/components/ui/button'
+import { cn } from '@/utils/style'
+
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="absolute flex min-h-full w-full items-center justify-center">
+      <div className="flex w-80 flex-col space-y-6 p-4">
+        <div className="flex items-center justify-center space-x-2">
+          <span className="flex size-9 items-center justify-center rounded-full bg-orange-9 bg-gradient-to-b from-white/5 to-black/10 text-white">
+            <span className="size-4">
+              <LogoIcon />
+            </span>
+          </span>
+          <span className="text-lg font-medium">zntask</span>
+        </div>
+
+        <div className="space-y-4">
+          <Link
+            href="/register"
+            className={cn(buttonStyle({ intent: 'primary' }), 'w-full')}
+          >
+            Register
+          </Link>
+          <Link
+            href="/login"
+            className={cn(buttonStyle({ intent: 'secondary' }), 'w-full')}
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
